f3c75f796233d950113661df0734dbc4504a5946
recep <recepa0125@gmail.com>
2020-11-19 19:41:02 -0700

    ikinci commit

--- src/day01/BirinciGun.java
+++ src/day01/BirinciGun.java
@@ -1,8 +1,15 @@
 package day01;
-
 public class BirinciGun {
-
 	public static void main(String[] args) {
+		
+	System.out.println("11:05 - Birinci Gun");	
+	System.out.println("Ikinci Commit");	
+		
+		
+		
+		
+		
+		
 		
 	}
 
